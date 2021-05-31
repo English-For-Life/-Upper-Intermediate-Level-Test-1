e9ac96c5899be3386bee4d7878bd710b429ed380
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-31 08:40:23 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I think it we've been talking ..... , it's time to start doing something.",
+        options:["long enough", "enough long", "such long", "so too long"],
+        answer: 0
+    },
+    {
+        q:"There ..... more green spaces in this city when I was a child.",
+        options:["was", "used to be", "would be", "used be"],
+        answer: 1
+    },
+    {
+        q:"Why ..... you always ..... late? It's so annoying!",
+        options:["do/come", "will/come", "are/coming", "would/come"],
+        answer: 2
+    },
+    {
+        q:"Wouldn't it be nice ..... travel to space on day?",
+        options:["could", "when you can", "to can", "to be able to"],
+        answer: 3
+    },
+    {
+        q:"I'm afraid you are not qualified ..... this position.",
+        options:["to get", "getting", "so you get", "get"],
+        answer: 0
+    },
+    {
+        q:"Your order ..... tomorrow morning.",
+        options:["is delivered", "is going to be delivered", "will deliver", "will have delivered"],
+        answer: 1
+    },
+    {
+     q:"The minister denied ..... the bribe.",
+        options:["to have taking", "he took", "taking", "to take"],
+        answer: 2   
+    },
+    {
+        q:"It's high time ..... about your future career.",
+        options:["you had thought", "you should think", "to think", "you thought"],
+        answer: 3
+    },
+    {
+        q:"I went home early because I needed to ..... .",
+        options:["get a tap repaired", "do tap repairing", "have repair a tap", "be repairing a tap"],
+        answer: 0
+    },
+    {
+        q:"I don't think he is capable ..... climbing that mountain.",
+        options:["in", "of", "at", "-"],
+        answer: 1
+    }
+]
